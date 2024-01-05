6363bd5827d2555121a84cd47e033b8ee3a24ef1
SebastianPrietoSophos <mario.prieto@sophossolutions.com>
2024-01-05 10:28:42 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-rumba-tours-services';
+  title = 'Substitute';
+  texto2 : string = ""
+  texto1 : string = "";
+  inputValue  : string = '';
+  
+  printOnTextArea (input : string){
+    console.log('Aqui ', input);
+
+    this.texto2 = input.replaceAll('bodySql.append("', "");
+    this.texto2 = this.texto2.replaceAll('");', "");
+
+  }
+
+  copyToClipboard () {
+    var copyText = document.getElementById("myOutput") as HTMLInputElement;
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+  }
+  
 }
+  
+
